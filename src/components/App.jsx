b04eccd3e8abb85ef52c4cb7c854369154c9cf21
Shandon <malcolm721@gmail.com>
2020-01-27 18:24:39 -0500

    Just starting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 // import components
+import Question from './Question';
 
 class App extends Component {
 
   render() {
     return (
+
       <div className="app">
-        Trivia!
+       <Question
+       question= "How old are you?"
+       answerOne= "2"
+       answerTwo="13"
+       answerThree="27"
+       answerFour="34"
+
+
+        />
+         <Question
+       question= "How old are you?"
+       answerOne= "2"
+       answerTwo="13"
+       answerThree="27"
+       answerFour="34"
+
+
+        />
+        
+        <Question
+       question= "How many children you have?"
+       answerOne= "2"
+       answerTwo="13"
+       answerThree="27"
+       answerFour="34"
+
+
+        />
+        
         
       </div>
     );
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 // import components
+import '../css/Question.css';
 
 
 class Question extends Component {
-  
+  // constructor(props){
+    
+
+
+  // }
   render() {
     return (
       <div>
-        Add your QuestionText, Reset Button and AnswerButtons here.  
+        <h2>{this.props.question}</h2>  
+        <button class="answer">{this.props.answerOne}</button>
+        <button class="answer">{this.props.answerTwo}</button>
+        <button class="answer">{this.props.answerThree}</button>
+        <button class="answer">{this.props.answerFour}</button>
+        <button>Reset</button>
       </div>
     );
   }
